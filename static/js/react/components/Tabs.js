a0de074e6992070488e03ccea80524fb84fa0e6f
Tyler Wickline <twick00@gmail.com>
2018-09-14 09:42:09 -0700

    Adds Tabs/Tab Component

---
+++ static/js/react/components/Tabs.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+export default class Tabs extends Component {
+    constructor(props) {
+        super(props);
+        console.log(props.children)
+        this.state = {
+            active: 0,
+        }
+    }
+    handleClick(i) {
+    this.setState({active: i})
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="btn-group" role="group" style={{
+                    display:"flex",
+                    wrap: "nowrap",
+                    justifyContent: "center",
+                    backgroundColor: "#868e96"
+                }}>
+                    {this.props.children.map((el, i) => {
+                        return <button onClick={() => this.setState({active: i})} key={i} type={"button"} className={"btn btn-secondary text-uppercase"} style={{width:"33.3%"}}>{el.props.children.props.id}</button>
+                    })}
+                </div>
+                {this.props.children.filter((el, i) => {
+                    if(this.state.active === i) {
+                        return el;
+                    }
+                })[0]}
+            </React.Fragment>
+        );
+    }
+}
+
+export const Tab = (props) => {
+    return (
+        <React.Fragment>
+            {props.children}
+        </React.Fragment>
+    );
+};
--- static/js/react/pages/PortfolioHomePage.js
+++ static/js/react/pages/PortfolioHomePage.js
@@ -16,12 +16,14 @@ import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import TypeItComponent from "../components/TypeItComponent";
 import TypeIt from "typeit";
+import Tabs, {Tab} from "../components/Tabs";
 
 class PortfolioHomePage extends Component {
     constructor() {
         super()
         console.log(image)
     }
+
     componentDidMount() {
 
     }
@@ -41,50 +43,71 @@ class PortfolioHomePage extends Component {
                     </NavbarLink>
                 </Navbar>
                 <Banner>
-                    <TypeItComponent strings={"I'M TYLER"} startDelay={500} cursor={false} breaklines={false} speed={120}/>
-                    <h6><TypeItComponent strings={["React Ninja", "Cool Guy", "Web Developer"]} breakLines={false} startDelete={true} nextStringDelay={2000} startDelay={3000} breaklines={false} speed={80} autoStart={false}/></h6>
+                    <TypeItComponent strings={"I'M TYLER"} startDelay={500} cursor={false} breaklines={false}
+                                     speed={120}/>
+                    <h6><TypeItComponent strings={["(Potential) React Ninja", "Web Developer"]} breakLines={false}
+                                         startDelete={true} nextStringDelay={2000} startDelay={3000} breaklines={false}
+                                         speed={80} autoStart={false}/></h6>
                 </Banner>
-                <Section id={"about"}>
-                    <div className="row align-items-center">
-                    <Column col={"col-12 col-md-6"}>
-                        <Heading>About Me</Heading>
-                        <SubHeading>I build and design fantastic websites and I love it!</SubHeading>
-                        <Paragraph>My passion ever since I was young has always been to build things.
-                            Its been a long road of getting my hands dirty but I appreciate every experience I've had
-                            getting here.
-                        </Paragraph>
-                        <Paragraph>
-                            When not programming, I like to spend time outside with friends
-                            and family hiking and kayaking. I also volunteer every year for the <a
-                            href="https://www.namiwa.org/index.php/namiwalks">National Alliance on Mental
-                            Illness</a>, which my brother-in-law is a spokesperson for!
-                            <small><a href="https://www.namiwa.org/index.php/donate">Please donate</a></small>
-                        </Paragraph>
-                        <a href="https://drive.google.com/file/d/14bnO4mfoM3R6aJHeI_LLv4UiIVZaRaq7/view?usp=sharing"
-                           target="_blank" className="m-btn" download>download resume</a>
-                    </Column>
-                    <Image src={image}/>
-                    </div>
-                </Section>
-                <Section id={"skills"} overlay={"dark"} classes={"bg-cover bg-fixed section-overlay bg-no-repeat bg-center-center"} style={{backgroundImage: "url(https://images.pexels.com/photos/1166657/pexels-photo-1166657.jpeg?cs=srgb&dl=book-bookcase-books-1166657.jpg&fm=jpg)"}}>
-                    <SkillsSection heading={"Some Of My Skills"} >
-                        <Skills>
-                            <Skill percent={"75%"}>HTML</Skill>
-                            <Skill percent={"55%"}>CSS</Skill>
-                            <Skill percent={"75%"}>JavaScript/ES6+</Skill>
-                            <Skill percent={"60%"}>Java</Skill>
-                            <Skill percent={"80%"}>React</Skill>
-                        </Skills>
-                    </SkillsSection>
-                </Section>
-                <Section id={"contact"}>
-                    <div className="row align-items-center text-center">
-                        <Contact icon={"ti-location-pin"} title={"Contact Address"} body={"Marysville, WA 98271"}/>
-                        <Contact icon={"ti-linkedin"} title={"My LinkedIn"} body={<a href="https://www.linkedin.com/in/tyler-wickline/">/in/tyler-wickline/</a>}/>
-                        <Contact icon={"ti-github"} title={"My Github"} body={<a href="https://github.com/twick00">https://github.com/twick00</a>}/>
-                        <Contact icon={"ti-email"} title={"My Email"} body={<a href="mailto:me@tylerwickline.com">me@tylerwickline.com</a>}/>
-                    </div>
-                </Section>
+                <Tabs>
+                    <Tab>
+                        <Section id={"about"}>
+                            <div className="row align-items-center">
+                                <Column col={"col-12 col-md-6"}>
+                                    <Heading>About Me</Heading>
+                                    <SubHeading>I build and design fantastic websites and I love it!</SubHeading>
+                                    <Paragraph>My passion ever since I was young has always been to build things.
+                                        Its been a long road of getting my hands dirty but I appreciate every experience
+                                        I've
+                                        had
+                                        getting here.
+                                    </Paragraph>
+                                    <Paragraph>
+                                        When not programming, I like to spend time outside with friends
+                                        and family hiking and kayaking. I also volunteer every year for the <a
+                                        href="https://www.namiwa.org/index.php/namiwalks">Natwional Alliance on Mental
+                                        Illness</a>, which my brother-in-law is a spokesperson for!
+                                        <small><a href="https://www.namiwa.org/index.php/donate">Please donate</a>
+                                        </small>
+                                    </Paragraph>
+                                    <a href="https://drive.google.com/file/d/14bnO4mfoM3R6aJHeI_LLv4UiIVZaRaq7/view?usp=sharing"
+                                       target="_blank" className="m-btn" download>download resume</a>
+                                </Column>
+                                <Image src={image}/>
+                            </div>
+                        </Section>
+                    </Tab>
+                    <Tab>
+                        <Section id={"skills"} overlay={"dark"}
+                                 classes={"bg-cover bg-fixed section-overlay bg-no-repeat bg-center-center"}
+                                 style={{backgroundImage: "url(https://images.pexels.com/photos/1166657/pexels-photo-1166657.jpeg?cs=srgb&dl=book-bookcase-books-1166657.jpg&fm=jpg)"}}>
+                            <SkillsSection heading={"Some Of My Skills"}>
+                                <Skills>
+                                    <Skill percent={"75%"}>HTML</Skill>
+                                    <Skill percent={"55%"}>CSS</Skill>
+                                    <Skill percent={"75%"}>JavaScript/ES6+</Skill>
+                                    <Skill percent={"60%"}>Java</Skill>
+                                    <Skill percent={"80%"}>React</Skill>
+                                </Skills>
+                            </SkillsSection>
+                        </Section>
+                    </Tab>
+                    <Tab>
+                        <Section id={"contact"}>
+                            <div className="row align-items-center text-center">
+                                <Contact icon={"ti-location-pin"} title={"Contact Address"}
+                                         body={"Marysville, WA 98271"}/>
+                                <Contact icon={"ti-linkedin"} title={"My LinkedIn"}
+                                         body={<a
+                                             href="https://www.linkedin.com/in/tyler-wickline/">/in/tyler-wickline/</a>}/>
+                                <Contact icon={"ti-github"} title={"My Github"}
+                                         body={<a href="https://github.com/twick00">https://github.com/twick00</a>}/>
+                                <Contact icon={"ti-email"} title={"My Email"}
+                                         body={<a href="mailto:me@tylerwickline.com">me@tylerwickline.com</a>}/>
+                            </div>
+                        </Section>
+                    </Tab>
+                </Tabs>
                 <Footer>
                     Both wickline.io&trade; and tylerwickline.com are owned and operated by Tyler Wickline
                 </Footer>
